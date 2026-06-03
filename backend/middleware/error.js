e8aaf2c7dc9af287f29c13307af48ecef67267e2
rabeya100X <rabeyanoor.dev@gmail.com>
2026-06-03 10:10:01 +0600

    feat: create custom ErrorResponse helper class

---
+++ backend/middleware/error.js
@@ -0,0 +1,10 @@
+class ErrorResponse extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+  error.message = err.message;
