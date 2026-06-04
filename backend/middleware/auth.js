93ce461045de26c851eb6414d93c6c128622df0e
rabeya100X <rabeyanoor.dev@gmail.com>
2026-06-04 18:53:42 +0600

    feat: implement jwt verification skeleton in protect middleware

---
+++ backend/middleware/auth.js
@@ -0,0 +1,10 @@
+const jwt = require('jsonwebtoken');
+const { ErrorResponse } = require('./error');
+const User = require('../models/User');
+
+const protect = async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
