dc0ceec67a171e608593157b2ad1ec017d1f1489
rabeya100X <rabeyanoor.dev@gmail.com>
2026-06-11 20:24:23 +0600

    feat: add abstract, authors and DOI to Paper schema

--- backend/models/Paper.js
+++ backend/models/Paper.js
@@ -8,3 +8,13 @@ const PaperSchema = new mongoose.Schema({
     maxlength: [200, 'Title cannot be more than 200 characters']
   },
   abstract: {
+    type: String,
+    required: [true, 'Please add an abstract/description']
+  },
+  authors: {
+    type: [String],
+    required: [true, 'Please add at least one author']
+  },
+  doi: {
+    type: String,
+    unique: true,
