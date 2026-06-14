02ca24ac39aab408a3839334b8fdbb563e577d45
rabeya100X <rabeyanoor.dev@gmail.com>
2026-06-14 18:28:45 +0600

    feat: define Comment schema and text length validation

---
+++ backend/models/Comment.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const CommentSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: [true, 'Please add some text'],
+    maxlength: [1000, 'Comment cannot be more than 1000 characters']
+  },
+  paper: {
+    type: mongoose.Schema.ObjectId,
