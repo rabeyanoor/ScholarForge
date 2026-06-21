8a04bc9968e5fdafe8f0345261edf8c6e16ec13a
rabeya100X <rabeyanoor.dev@gmail.com>
2026-06-21 19:44:13 +0600

    feat: filter out direct query keys from query string

--- backend/controllers/paperController.js
+++ backend/controllers/paperController.js
@@ -8,3 +8,13 @@ exports.getPapers = async (req, res, next) => {
   try {
     let query;
     const reqQuery = { ...req.query };
+
+    // Fields to exclude from direct filtering
+    const removeFields = ['select', 'sort', 'page', 'limit', 'search'];
+    removeFields.forEach(param => delete reqQuery[param]);
+
+    // Create query string
+    let queryStr = JSON.stringify(reqQuery);
+
+    // Create operators ($gt, $gte, etc)
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
